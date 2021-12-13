eba97acdb2df1112839034b99fe5e2ea6eabf57d
Kaique Moreira <kaiquemoreiracloud@gmail.com>
2021-12-13 16:32:25 -0300

    Add pytest in exercise 2

--- src/exercise2/consonants.py
+++ src/exercise2/consonants.py
@@ -1,15 +1,18 @@
 """ Faça um Programa que leia uma string e diga quantas consoantes foram lidas.
  Imprima as consoantes """
 
-def consonants():
-    palavra = str(input("Digite uma palavra: "))
+def consonants(palavra: str):
+    return palavra 
+
+
+if __name__ == '__main__':
+    palavra = consonants("kaique")
+
     count = 0
 
     for letra in palavra:
-        if letra in "bcdfghjklmnpqrstvxywz":
-            count += 1
-            print(f"As consoantes são: {letra}")
-
-    print(f"A palavra '{palavra}' tem {count} consoantes.")
-                 
-consonants()
+            if letra in "bcdfghjklmnpqrstvxywz":
+                count += 1
+                print(f"As consoantes são: {letra}")
+    print(f"As quantidades de consoantes são: {count}")
+    
---
+++ tests/exercise2/test_consonants.py
@@ -0,0 +1,6 @@
+import pytest
+from src.exercise2.consonants import consonants
+
+
+def test_consonants():
+    assert consonants("kaique")
